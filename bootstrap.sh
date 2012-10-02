5c27ca469430577ae474514e6aa482912f33a52b
Hybrid Cluster <hybrid@demo2.elastic-sites.com>
2012-10-02 15:41:46 +0000

    Nice for the next person

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,7 @@
 pkg_add -r python26
 pkg_add -r ezjail
 
+# XXX Fetch the correct tarball depending
+fetch ftp://ftp.freebsd.org/pub/FreeBSD/ports/amd64/packages-8.2-release/Latest/python26.tbz
+
 /usr/local/bin/python zfs-perf-test.py
