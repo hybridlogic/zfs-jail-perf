a9ca3ec81bd08afe1fdb4c4a2e95ae15f4c87253
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2012-10-30 12:45:41 -0400

    this does not work, but I do not know how to fix it yet

--- graph-results.py
+++ graph-results.py
@@ -1,14 +1,20 @@
 import sys, pickle
 
 import matplotlib.pyplot as plt
+from matplotlib import legend
 
 def main(filename):
     with open(filename) as fObj:
         measurements = pickle.load(fObj)
 
+    artist = []
+    label = []
     for dataset in ['read', 'write', 'jail_read', 'jail_write', 'loaded_read', 'loaded_write', 'loaded_jail_read', 'loaded_jail_write']:
         [line] = plt.plot(measurements[dataset + '_measurements'])
         line.label = dataset
+        artist.append(line)
+        label.append(dataset)
+    legend(artist, label)
     plt.ylabel('seconds')
     plt.show()
 
